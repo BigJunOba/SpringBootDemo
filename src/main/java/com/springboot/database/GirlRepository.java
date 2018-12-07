d11387c1fb53dadfd18114db66a716820f20d71e
BigJunOba <sjy534948129@sina.com>
2018-12-07 14:26:09 +0800

    新增自定义查询方法

--- src/main/java/com/springboot/database/GirlRepository.java
+++ src/main/java/com/springboot/database/GirlRepository.java
@@ -3,7 +3,11 @@ package com.springboot.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GirlRepository extends JpaRepository<Girl, Integer> {
 
+    //通过年龄来查询
+    public List<Girl> findByAge(Integer age);
 }
