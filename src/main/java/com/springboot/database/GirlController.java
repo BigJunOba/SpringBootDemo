55146b95bdee6bf4723030b4b73bc3720321071a
BigJunOba <sjy534948129@sina.com>
2018-12-07 14:31:51 +0800

    新增调用Service类的Controller

--- src/main/java/com/springboot/database/GirlController.java
+++ src/main/java/com/springboot/database/GirlController.java
@@ -1,9 +1,9 @@
 package com.springboot.database;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -12,9 +12,42 @@ public class GirlController {
     @Autowired
     private GirlRepository girlRepository;
 
+    @Autowired
+    private GirlService girlService;
+
     @GetMapping(value = "/girls")
     public List<Girl> girlsList() {
         return girlRepository.findAll();
+    }
+
+    @GetMapping(value = "/girls/query")
+    public Girl girlFindOne(@PathParam("id") Integer id) {
+        return girlRepository.findOne(id);
+    }
+
+    @PostMapping(value = "/girls/update/{id}")
+    public Girl girlUpdateOne(@PathVariable("id") Integer id, @PathParam("cupSize") String cupSize, @PathParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+        girl.setId(id);
+        return girlRepository.save(girl);
+    }
+
+    @DeleteMapping("/girls/delete/{id}")
+    public void deleteOne(@PathVariable("id") Integer id){
+        girlRepository.delete(id);
+    }
+
+    //通过年龄查询女生列表
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
+        return girlRepository.findByAge(age);
+    }
+
+    @PostMapping(value = "/girls/two")
+    public void girlTwo() {
+        girlService.insertTwo();
     }
 
 }
